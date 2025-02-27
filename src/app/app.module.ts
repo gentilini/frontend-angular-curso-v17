983beba6e2b8dd2d8589292986e4698ffda2963e
Rafael Gentilini <gentilini.ads@gmail.com>
2025-02-27 15:34:47 -0300

    configuração de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BaseUiComponent } from './components/base-ui/base-ui.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BaseUiComponent } from './components/base-ui/base-ui.component';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-    BaseUiComponent
+    BaseUiComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
